c2b986bc73d5f0642032714bc3dacc338a48c20b
Tobias Rohnstock <tobias.rohnstock@live-eo.com>
2025-04-01 14:33:08 +0100

    remove nox

--- stapi-fastapi/noxfile.py
+++
@@ -1,7 +0,0 @@
-import nox
-
-
-@nox.session(python=["3.12", "3.13"])
-def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "pytest", external=True)
