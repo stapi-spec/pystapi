84528ec07de158c2a96d9ca2c00bdcf9f145886f
Tobias Rohnstock <tobias.rohnstock@live-eo.com>
2025-04-01 12:21:23 +0100

    remove custom bins for mypy and ruff

--- bin/run-mypy.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeuo pipefail
-# set -x # print each command before executing
-
-MYPYPATH=src mypy src/ tests/
--- bin/run-ruff.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeuo pipefail
-# set -x # print each command before executing
-
-ruff check --fix # lint python files
-ruff format # format python files
