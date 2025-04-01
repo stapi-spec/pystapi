03a096ac628f82ba73703cc9a53f1b01fb659e65
dylan bartels <dylan.bartels@live-eo.com>
2025-04-01 16:54:32 +0100

    Rename to pystapi-validator

--- pystapi-validator/src/pystapi_validator/__init__.py
+++ pystapi-validator/src/pystapi_validator/__init__.py
@@ -2,7 +2,7 @@ import sys
 import pytest
 
 def main() -> None:
-    sys.exit(pytest.main(['stapi-validator/tests/validate_api.py']))
+    sys.exit(pytest.main(['pystapi-validator/tests/validate_api.py']))
 
 if __name__ == '__main__':
     main()
