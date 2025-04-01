625f28d94f402092a0265be9672a55964437f411
Philip Weiss <philip.weiss@orbitalsidekick.com>
2025-04-01 07:42:07 -0700

    add opportunity models to exports

--- stapi-pydantic/src/stapi_pydantic/__init__.py
+++ stapi-pydantic/src/stapi_pydantic/__init__.py
@@ -1,10 +1,18 @@
-from .opportunity import OpportunityProperties
+from .opportunity import Opportunity, OpportunityCollection, OpportunityPayload, OpportunityProperties, \
+    OpportunitySearchRecord, OpportunitySearchRecords, OpportunitySearchStatus, OpportunitySearchStatusCode
 from .product import Product, Provider, ProviderRole
 from .shared import Link
 
 __all__ = [
     "Link",
+    "Opportunity",
+    "OpportunityCollection",
+    "OpportunityPayload",
     "OpportunityProperties",
+    "OpportunitySearchRecord",
+    "OpportunitySearchRecords",
+    "OpportunitySearchStatus",
+    "OpportunitySearchStatusCode",
     "Product",
     "Provider",
     "ProviderRole",
