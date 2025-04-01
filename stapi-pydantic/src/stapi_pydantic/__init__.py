e5b52c9553f43625c9b27071a827e958d23d227d
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-01 16:43:56 +0100

    fix: lint

--- stapi-pydantic/src/stapi_pydantic/__init__.py
+++ stapi-pydantic/src/stapi_pydantic/__init__.py
@@ -1,5 +1,13 @@
-from .opportunity import Opportunity, OpportunityCollection, OpportunityPayload, OpportunityProperties, \
-    OpportunitySearchRecord, OpportunitySearchRecords, OpportunitySearchStatus, OpportunitySearchStatusCode
+from .opportunity import (
+    Opportunity,
+    OpportunityCollection,
+    OpportunityPayload,
+    OpportunityProperties,
+    OpportunitySearchRecord,
+    OpportunitySearchRecords,
+    OpportunitySearchStatus,
+    OpportunitySearchStatusCode,
+)
 from .product import Product, Provider, ProviderRole
 from .shared import Link
 
