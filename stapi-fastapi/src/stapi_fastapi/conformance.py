ef78f4db22b506ab1d9d6c3a3d04243977845d2a
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-03 16:07:34 +0100

    --wip-- [skip ci]

--- stapi-fastapi/src/stapi_fastapi/conformance.py
+++ stapi-fastapi/src/stapi_fastapi/conformance.py
@@ -1,5 +1,17 @@
 from stapi_pydantic.constants import STAPI_VERSION
 
+# API-level Conformance Classes
 CORE = f"https://stapi.example.com/v{STAPI_VERSION}/core"
+ORDER_STATUSES = f"https://stapi.example.com/v{STAPI_VERSION}/order-statuses"
+SEARCHES_OPPORTUNITY = f"https://stapi.example.com/v{STAPI_VERSION}/searches-opportunity"
+SEARCHES_OPPORTUNITY_STATUSES = f"https://stapi.example.com/v{STAPI_VERSION}/searches-opportunity-statuses"
+
+# Product Conformance Classes
 OPPORTUNITIES = f"https://stapi.example.com/v{STAPI_VERSION}/opportunities"
-ASYNC_OPPORTUNITIES = f"https://stapi.example.com/v{STAPI_VERSION}/async-opportunities"
+OPPORTUNITIES_ASYNC = f"https://stapi.example.com/v{STAPI_VERSION}/opportunities-async"
+GEOJSON_POINT = "https://geojson.org/schema/Point.json"
+GEOJSON_LINESTRING = "https://geojson.org/schema/Linestring.json"
+GEOJSON_POLYGON = "https://geojson.org/schema/Point.json"
+GEOJSON_MULTI_POINT = "https://geojson.org/schema/Point.json"
+GEOJSON_MULTI_POLYGON = "https://geojson.org/schema/Point.json"
+GEOJSON_MULTI_LINESTRING = "https://geojson.org/schema/Point.json"
