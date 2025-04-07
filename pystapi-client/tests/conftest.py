3a7aa277fd2547aa351e6a36cc50c98403054dc4
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-07 07:33:52 -0600

    refactor: opinionated tweaks to pystapi-client tests

--- pystapi-client/tests/conftest.py
+++ pystapi-client/tests/conftest.py
@@ -1,37 +1,35 @@
 import json
-from collections.abc import Generator
+from collections.abc import Iterator
 from copy import deepcopy
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
-import httpx
 import pytest
 import respx
-from httpx import Response
+from httpx import Request, Response
+from respx import MockRouter
 
 WORKING_DIR = Path(__file__).parent
 
 
 def load_fixture(name: str) -> dict[str, Any]:
     with open(WORKING_DIR / "fixtures" / f"{name}.json") as f:
-        return json.load(f)  # type: ignore[no-any-return]
+        return cast(dict, json.load(f))
 
 
 @pytest.fixture
-def mocked_api() -> Generator[respx.MockRouter, None, None]:
+def api() -> Iterator[MockRouter]:
     landing_page_data = load_fixture("landing_page")
     products = load_fixture("products")
 
-    with respx.mock(base_url="https://stapi.example.com", assert_all_called=False) as respx_mock:
+    with respx.mock(base_url="http://stapi.test", assert_all_called=False) as respx_mock:
         landing_page = respx_mock.get("/")
         landing_page.return_value = Response(200, json=landing_page_data)
 
         conformance_route = respx_mock.get("/conformance")
         conformance_route.return_value = Response(200, json={"conformsTo": landing_page_data["conformsTo"]})
 
-        # products_route.return_value = Response(200, json=products)
-
-        def mock_products_response(request: httpx.Request) -> httpx.Response:
+        def mock_products_response(request: Request) -> Response:
             products_limited = deepcopy(products)
             limit = request.url.params.get("limit")
             page = int(request.url.params.get("page", 1))
@@ -43,7 +41,7 @@ def mocked_api() -> Generator[respx.MockRouter, None, None]:
                 if has_next_page:
                     products_limited["links"].append(
                         {
-                            "href": "https://stapi.example.com/products?limit=1&page=2",
+                            "href": "http://stapi.test/products?limit=1&page=2",
                             "method": "GET",
                             "rel": "next",
                         }
