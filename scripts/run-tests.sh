f7c0743afe1181807b9fe98ec0bf9fcfd78ef5f7
Phil Varner <phil.varner@orbitalsidekick.com>
2025-12-16 11:04:08 -0500

    add echo in test run script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,7 @@ for path in stapi-fastapi pystapi-validator pystapi-client stapi-pydantic; do
   name=$(basename "$path")
 
   set +e
+  echo "Running tests for package $name"
   uv sync --package "$name"
   uv run --package "$name" --directory "$path" pytest -p no:sugar
   code=$?
@@ -17,3 +18,7 @@ for path in stapi-fastapi pystapi-validator pystapi-client stapi-pydantic; do
     *)   echo "   pytest failed in $name (exit $code)"; exit "$code";;
   esac
 done
+
+# finally just globally sync, so we don't just have the stapi-pydantic deps
+# since this script will commonly be used before commit, and pre-commit is needed then
+uv sync
